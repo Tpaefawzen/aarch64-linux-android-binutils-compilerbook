083bff87cace95926dff0038a62d8369c020d509
Tpaefawzen <100011199+Tpaefawzen@users.noreply.github.com>
2025-04-24 06:02:39 +0900

    Still TODO parseZU8

--- parseZU8.s
+++ parseZU8.s
@@ -1,35 +1,34 @@
 //! Try to parse [:0]const u8 as unsigned decmial integer as u8.
 .text
 .globl parseZU8
-
 /// input x0 [*:0]const u8
 /// Output x0 i64
 /// If parsed 0<=n<=255 otherwise n<0
 parseZU8:
 mov x12,10 // const
 mov x11,0 // result
-
-ldr x1,[x0]
-cbnz x1,.Loop
-.L.invalChar:
-.L.overflow:
-mov x11,-1
-.L.ok:
-mov x0,x11
-ret
-
-.Loop:
-mov x9,x0
-ldr x0,[x9]
+sub x9,x0,1
+ldr x0,[x0]
+// check for character
+mov x1,x0
+cbz x0,.L.invalChar
+.L.loop:
+add x9,x0,1
+mov x0,x9
+ldr x0,[x0]
+cbz x1,.L.ok
 bl parseDigit
 cmp x0,0
 b.mi .L.invalChar
-
-madd x11,x11,x12,x0
-cmp x11,0xff
-b.gt .L.overflow
-
-add x0,x0,1
-ldr x1,[x0]
-cbz x1,.L.ok
-b .Loop
+madd x11,x11,x10,x0
+cmp x11,256
+b.ge .L.overflow
+b .L.loop
+.L.invalChar:
+mov x0,-1
+ret
+.L.overflow:
+mov x11,-2
+.L.ok:
+mov x0,x11
+ret
