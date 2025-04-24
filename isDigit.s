6792a09ff6432f8b6d22d46a1bddf80b9bb56444
Tpaefawzen <100011199+Tpaefawzen@users.noreply.github.com>
2025-04-24 21:25:47 +0900

    Rewrite isDigit
    subs ccmp

--- isDigit.s
+++ isDigit.s
@@ -1,12 +1,10 @@
+//! '0'<=c and c<='9'
 /// x0 u8
 /// return x0 u1 // 1 for truthy
 .globl isDigit
 isDigit:
-and w0,w0,255
-mov x1,xzr
-cmp w0,48
-cinc x1,x1,pl
-cmp w0,58
-cinc x1,x1,pl
-and x0,x1,1
+uxtb x0,w0
+subs x0,x0,48
+ccmp x0,10,0,pl
+cset x0,lt
 ret
