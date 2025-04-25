e33d68d6cfe03607412cd4152919779c42385735
Tpaefawzen <100011199+Tpaefawzen@users.noreply.github.com>
2025-04-25 19:46:30 +0900

    9cc.s Try to begin codegen

--- 9cc.s
+++ 9cc.s
@@ -22,11 +22,25 @@ bl szToSlice
 // x0 = argv[1]: [*]const u8
 // x1 = x0.len
 //
-stp x0,x1,[sp,-16]!
+mov x19,x0
+mov x20,x1
 bl isInt
 cbz x0,errorNotInt
-ldp x0,x1,[sp],16
+
+ldr x0,=.const._start
+mov x1,6
+bl codegen.genGlobl
+ldr x0,=.const._start
+mov x1,6
+bl codegen.genLabel
+mov x0,x19
+mov x1,x20
+bl codegen.genMov
+bl codegen.genRet
 
 mov x0,0
 mov x8,93
 svc 0
+
+.data
+.const._start: .ascii "_start"
