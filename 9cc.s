887b9cf91b0fb1461452188ffb32d76288e947c2
Tpaefawzen <100011199+Tpaefawzen@users.noreply.github.com>
2025-04-26 20:17:19 +0900

    9cc.s: skip step 2 and do step 3

--- 9cc.s
+++ 9cc.s
@@ -3,64 +3,44 @@
 .text
 .globl _start
 _start:
-//
-// [sp] is argc: i32
-// [sp+8] is argv[0]: ?[*]const u8
-// [sp+16] is argv[1]: ?[*]const u8
-// ...
-//
 ldr x0,[sp]
-//
-// x0: i32 is argc
-//
 cmp x0,2
 b.lt errorUsage
 
-bl genPreamble
-
 ldr x0,[sp,16]
 bl szToSlice
+ldr x8,=.L.my_tokenizer
+bl tokenizer.Tokenizer.newFromSlice
 //
-// x0 = argv[1]: [*]const u8
+// x0: [*]const u8
 // x1 = x0.len
+// x8: Tokenizer
 //
-bl sliceInt
-cbz x1,bl.errorNotInt
 stp x0,x1,[sp,-16]!
-bl parseIntU32
-cbz x1,errorOverflow
-mov x2,x1
+str x8,[sp,-8]!
+bl genPreamble
+ldr x8,[sp],8
 ldp x0,x1,[sp],16
-bl sliceSlice
 
-.L.parse:
-cbz x1,.L.finally
-stp x0,x1,[sp,-16]!
-
-ldr x19,[sp]
-ldr x20,[x19]
-
-cmp x20,0x2B // plus sign
-b.eq .L.genAdd
-cmp x20,0x2D // minus sign
-b.eq .L.genSub
-cbz x21,errorUnkChar
-
-.L.genAdd:
-add x0,x0,1
-sub x1,x1,1
-bl tryGenAdd
+bl tryExpectNumber
 //
-// x0,x1 is updated
-b .L.parse
-
-.L.genSub:
-add x0,x0,1
-sub x1,x1,1
-bl tryGenSub
+// x0: i64
+// x8 Tokenizer
 //
-// x0,x1 is updated
-b .L.parse
+str x8,[sp,-8]!
+bl gen.mov
+ldr x8,[sp],8
+
+.L.addOrSub:
+bl atEof
+//
+// x0: u1 1 for truthy
+cbnz x0,.L.finally
+ldr x8,.L.my_tokenizer
+bl consumePlus
+//
+//.
+,n
 
 .L.finally:
 bl genPostamble
@@ -68,3 +48,8 @@ bl genPostamble
 mov x0,0
 mov x8,93
 svc 0
+
+.bss
+
+.L.my_tokenizer:
+.fill 16 // bytes
